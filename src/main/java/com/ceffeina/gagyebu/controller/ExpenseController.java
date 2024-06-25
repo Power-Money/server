ac0bcb8c51cf6956374214642b86eccadda8af9b
Ceffiena <ceffeina@gmail.com>
2024-06-25 10:26:07 +0900

    fix: 오류 수정

--- src/main/java/com/ceffeina/gagyebu/controller/ExpenseController.java
+++ src/main/java/com/ceffeina/gagyebu/controller/ExpenseController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.ceffeina.gagyebu.dto.ExpenseDTO;
 import com.ceffeina.gagyebu.dto.PageRequestDTO;
@@ -18,54 +21,41 @@ public class ExpenseController {
 
     private final ExpenseService service;
 
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/expense/list";
-    }
-
     @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model model) {
-        log.info("list: " + pageRequestDTO);
-        model.addAttribute("result", service.getList(pageRequestDTO));
+    public String list(PageRequestDTO pageRequestDTO, Model model) {
+        model.addAttribute("pageRequestDTO", pageRequestDTO);  // PageRequestDTO를 모델에 추가
+        model.addAttribute("pageResultDTO", service.getList(pageRequestDTO)); // PageResultDTO를 모델에 추가
+        return "list";  // templates/list.html 파일을 참조
     }
 
     @GetMapping("/register")
-    public void register() {
+    public void register(){
     }
 
     @PostMapping("/register")
     public String registerPost(ExpenseDTO dto, RedirectAttributes redirectAttributes) {
-        log.info("dto... " + dto);
         Long eno = service.register(dto);
-        redirectAttributes.addFlashAttribute("msg", eno);
+        redirectAttributes.addFlashAttribute("msg", eno + " 등록되었습니다.");
         return "redirect:/expense/list";
     }
 
     @GetMapping({"/read", "/modify"})
-    public void read(@RequestParam("eno") Long eno,
-                     @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
-                     Model model) {
-        log.info("eno: " + eno);
+    public void read(long eno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
         ExpenseDTO dto = service.read(eno);
         model.addAttribute("dto", dto);
     }
 
     @PostMapping("/remove")
-    public String remove(@RequestParam("eno") Long eno, RedirectAttributes redirectAttributes) {
-        log.info("eno: " + eno);
+    public String remove(long eno, RedirectAttributes redirectAttributes) {
         service.remove(eno);
-        redirectAttributes.addFlashAttribute("msg", eno);
+        redirectAttributes.addFlashAttribute("msg", eno + " 삭제되었습니다.");
         return "redirect:/expense/list";
     }
 
     @PostMapping("/modify")
-    public String modify(ExpenseDTO dto,
-                         @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
-                         RedirectAttributes redirectAttributes) {
-        log.info("dto: " + dto);
+    public String modify(ExpenseDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
         service.modify(dto);
-        redirectAttributes.addAttribute("page", requestDTO.getPage());
-        redirectAttributes.addAttribute("eno", dto.getEno());
-        return "redirect:/expense/read";
+        redirectAttributes.addFlashAttribute("msg", dto.getEno() + " 수정되었습니다.");
+        return "redirect:/expense/list";
     }
 }
--- src/main/java/com/ceffeina/gagyebu/dto/PageRequestDTO.java
+++ src/main/java/com/ceffeina/gagyebu/dto/PageRequestDTO.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+
 @Data
 @Builder
 @AllArgsConstructor
